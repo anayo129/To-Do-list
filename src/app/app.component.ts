8a48dcd1ef319d467a6ba6fae40e62f698bca1c2
anayo129 <j1c2m3r4@gmail.com>
2023-02-10 14:32:58 -0500

    desarrollo lista de tareas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,30 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   taskName: string = '';
+//   tasks: string[] = [];
+
+//   addTask() {
+//     this.tasks.push(this.taskName);
+//     this.taskName = '';
+//   }
+
+//   deleteTask(index: number) {
+//     this.tasks.splice(index, 1);
+//   }
+
+//   updateTask(index: number) {
+//     this.taskName = this.tasks[index];
+//     this.tasks.splice(index, 1);
+//   }
+// }
+
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +32,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'toDo-app';
+
+  tasks: string[] = [];
+  newTask: string = '';
+  editTaskData = '';
+  editedTaskIndex = -1;
+
+  addTask() {
+    this.tasks.push(this.newTask);
+    this.newTask = '';
+  }
+
+  removeTask(task: string) {
+    const index = this.tasks.indexOf(task);
+    this.tasks.splice(index, 1);
+  }
+
+  editTask(task: string) {
+    // this.editTaskData = task;
+    // this.editedTaskIndex = this.tasks.indexOf(task);
+    this.editTaskData = task
+      
+  }
+
+  updateTask() {
+    const index = this.tasks.indexOf(this.editTaskData);
+    this.tasks[index] = this.newTask;
+    this.editTaskData = '';
+    this.newTask = '';
+    }
+    
+    cancelEditTask() {
+    this.editTaskData = '';
+    this.newTask = '';
+    }
+
+  // updateTask() {
+  //   this.tasks[this.editedTaskIndex] = this.editTaskData;
+  //   this.cancelEditTask();
+  // }
+
+  // cancelEditTask() {
+  //   this.editTaskData = '';
+  //   this.editedTaskIndex = -1;
+  // }
 }
+
+
+
